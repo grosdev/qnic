7749a084321f75616bf41b0e9e86a176e4607e69
Logan Schmidt <logo3801@gmail.com>
2014-08-20 08:50:22 -0500

    Uploaded current working file
    Bash script I wrote.

---
+++ qnic.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Developed by Logan Schmidt
+# for work at SynerComm
+# 
+# Free to modify and use, MIT License
+# 
+clear
+start_test() {
+echo "---------------------"
+echo "----Test--started----"
+echo "---------------------"
+date -u
+echo "By:"
+echo $USER
+echo "---------------------"
+echo "---Open ports are----"
+echo "---------------------"
+nmap -v -sV -T4 $test_url | grep -n "tcp"
+echo "---------------------"
+echo "---Server--Supports--"
+echo "---------------------"
+sslscan $test_url | grep -n "Accepted"
+echo "---------------------"
+echo "---Is it Wordpress?--"
+echo "---------------------"
+echo "-There is a bug here-"
+echo "---pressing ENTER----"
+echo "-usually fixes this.-"
+echo "---------------------"
+wpscan --url $test_url | grep -n "[!]"
+echo "---------------------"
+echo "---Finding DIRs------"
+echo "---------------------"
+dirb http://$test_url -f | grep -n "-"
+echo "---------------------"
+echo "---TEST--COMPLETE----"
+echo "---------------------"
+echo "-More to come later--"
+echo "------d4rkd0s--------"
+echo "---------------------"
+exit
+}
+echo "Welcome to d4rkd0s's pentest scanner"
+echo "This uses nmap, sslscan, wpscan, dirb."
+read -p "Please enter the URL to test [sub.domain.tld]: " test_url
+echo "You entered: $test_url"
+read -p "Do you have permission to test this URL? (y/n) " yn
+while true; do
+	case $yn in
+		[Yy]* ) start_test;;
+		[Nn]* ) exit;;
+		* ) echo "Please enter either y or n...";;
+	esac
+done
